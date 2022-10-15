ff04174d38279b6588c86eae07a2d3baca91a70d
valentin.oprea@ferendia.com <aeroprea@gmail.com>
2022-10-15 20:00:49 +0300

    play with security

---
+++ src/main/java/com/valisGroup/rest/webservices/restfulwebservices/securityConfig/SpringSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.valisGroup.rest.webservices.restfulwebservices.securityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+}
